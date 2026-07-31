8b4640f087a073fedb3824bb82cbcf7df8c97646
Tharun Hrithick SR <tharunhrithicksr@Tharuns-MacBook-Air-2.local>
2026-07-31 18:28:21 +0530

    Update configurations

--- api/index.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pathlib import Path
-
-# Ensure project root is in python path
-root_dir = Path(__file__).resolve().parent.parent
-if str(root_dir) not in sys.path:
-    sys.path.insert(0, str(root_dir))
-
-from app.main import app
